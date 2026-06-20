8f6128a151060701bfb86307bcefb4eb4f0fe10d
shahdeep1399 <shahdeep1399@gmail.com>
2026-06-19 23:07:17 -0400

    linux issue resolved

--- yolov5/detect.py
+++ yolov5/detect.py
@@ -35,7 +35,8 @@ import sys
 from pathlib import Path
 
 import torch
-
+import pathlib
+pathlib.WindowsPath = pathlib.PosixPath
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
 if str(ROOT) not in sys.path:
